fdcdd437b5d23d1da1014ce3e698d73d9689b172
dorobatmihai <dorobat.mihai@ivfa.ro>
2025-03-16 13:03:23 +0200

    React flow grouping implementation
    - added a new component 'GroupNode' responsible for connection with the other nodes with all around options to connect
    - created a common utils directory where all the styles were defined and exported into an index file for a better code quality
    - updated main component of flow-canvas along with some new functionalities like adding note to main group or separate
    - styled the ReactFlow props bases on MiniMap,Background
    - new onConnect function used everytime when nodes are getting connected (managing source node and target )
    - useCallback  usage to avoid any other re-renders
    - dynamic node creation implemented along with some pre-defined locations and draggable option based on ReactFlow and onNodesChange

---
+++ src/utils/styles.ts
@@ -0,0 +1,8 @@
+export const COLORS = {
+  groupBorder: '#007bff',
+  groupBackground: '#ece4e4',
+  nodeStroke: '#ff0072',
+  nodeBackground: '#ffdde2',
+  minimapBackground: '#f3f4f6',
+  backgroundGrid: '#9ca3af'
+}
